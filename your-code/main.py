91aa502fad7b95db5f7691ed64cac0d4bcdd8f5d
sujeto123 <adrianpradomart@gmail.com>
2022-03-28 18:14:01 +0200

    Update main.py

--- your-code/main.py
+++ your-code/main.py
@@ -1,66 +1,105 @@
 #1. Import the NUMPY package under the name np.
+from re import T
+import numpy as np 
+
 
 
 
 #2. Print the NUMPY version and the configuration.
+
+print(np.__version__)
+#1.22.3
 
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
+a=np.random.random([2,3,5])
+
 
 
 #4. Print a.
 
+print(a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
+b=np.random.random([5,2,3])
+
+b=np.ones_like(b)
 
 
 #6. Print b.
 
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
+#YEP,
 
+print(np.size(a))
+
+print(np.size(b))
 
 
 #8. Are you able to add a and b? Why or why not?
 
+#YEP, no se puede por no tener la misma dimension
 
+#np.add (a,b)
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
+
+c=b.reshape([2,3,5])
+
+
 
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
+d=a+c
+
+print(d)
+
+#Funciona porque ahora tienen las mismas dimensiones despues de haber aplicado el reshape
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
+print(a)
+print(d)
 
+#d is bigger than a
 
 
 #12. Multiply a and c. Assign the result to e.
 
+e=np.multiply(a,c)
+
+print(e)
 
 
 #13. Does e equal to a? Why or why not?
+
+#Yes, because 
 
 
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
-
+d_max=np.max(d)
+d_min=np.min(d)
+d_mean=np.mean(d)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
+
+f=np.empty([2,3,5])
+
 
 
 
