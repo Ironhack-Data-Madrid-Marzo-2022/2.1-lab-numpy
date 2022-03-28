8fd32666c3135f8abc9d2f4b25ca58108cbcc710
trabalong <gastraba@gmail.com>
2022-03-28 17:46:42 +0200

    Primer commit

--- your-code/main.py
+++ your-code/main.py
@@ -1,82 +1,113 @@
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-
+print(np.version.version)
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
+'''np.random.rand(n)
 
+np.random.randint(n, size=m)
+
+np.random.random(n)'''
+
+a = np.random.rand(2,3,5)
 
 #4. Print a.
 
+print(a)
 
+#5. Create a 5x2x3 3-dimensional array with all values equaling 1. #Assign the array to variable "b"
 
-#5. Create a 5x2x3 3-dimensional array with all values equaling 1.
-#Assign the array to variable "b"
-
-
+b = np.ones(((5,2,3)))
 
 #6. Print b.
 
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
+if a.ndim==b.ndim: print('a and be have the same size')
 
-
+else: print('Nop')
 
 #8. Are you able to add a and b? Why or why not?
 
-
+'''No, I'm not because operands could not be broadcast together with shapes (2,3,5) (5,2,3)'''
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c = b.reshape(((2, 3, 5)))
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
+d = np.add(a,c)
 
+'''It works because a and c have the same structure-number of rows and columns'''
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
+print(a)
 
+'''The difference and relation between the two arrays is 1.0'''
 
+print(d)
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e = np.multiply(a,c)
 
 #13. Does e equal to a? Why or why not?
 
+if e.all()==a.all(): print('e and a are equals')
 
+else: print("They aren't")
 
+'''Because they have the same dimensions and elements'''
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_max = d.max()
 
+d_min = d.min()
 
+d_mean = d.mean()
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
+f = np.empty([2,3,5])
 
-
-
-"""
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
-If a value in d is larger than d_mean but smaller than d_max, assign 75 to the corresponding value in f.
+'''If a value in d is larger than d_mean but smaller than d_max, assign 75 to the corresponding value in f.
 If a value equals to d_mean, assign 50 to the corresponding value in f.
 Assign 0 to the corresponding value(s) in f for d_min in d.
 Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
-Note: you don't have to use Numpy in this question.
-"""
+Note: you don't have to use Numpy in this question.'''
 
+for i,j in enumerate(d):
+    
+    for k,l in enumerate(j):
+        
+        for m,n in enumerate(l):                      
 
+                if n>d_min and n<d_mean: f[i][k][m] = 25
 
+                elif n>d_mean and n<d_max: f[i][k][m] = 75
+
+                elif n==d_mean: f[i][k][m] = 50
+
+                elif n==d_min: f[i][k][m] = 0
+
+                elif n==d_max: f[i][k][m] = 100
+
+                else: f[i][k][m] = None
+
+print(f)
 
 """
 #17. Print d and f. Do you have your expected f?
@@ -98,6 +129,13 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
+
+print(d)
+
+print(f)
+
+
+
 
 
 """
