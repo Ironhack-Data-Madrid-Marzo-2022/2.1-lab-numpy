c003810583b23be30777b3b942330153116c8b7b
Migue Manzano <miguelmanzan@gmail.com>
2022-03-28 23:16:17 +0200

    Solve last exercises

--- your-code/main.py
+++ your-code/main.py
@@ -2,14 +2,16 @@
 import numpy as np
 
 #2. Print the NUMPY version and the configuration.
+print("\nEXERCISE 2 - Print the NUMPY version and the configuration\n")
 print(np.__version__)
-# print(np.show_config())
+print(np.show_config())
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 a=np.random.random((2, 3, 5))
 
 #4. Print a.
+print("\nEXERCISE 4 - Print a\n")
 print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
@@ -17,54 +19,56 @@ print(a)
 b = np.ones((5, 2, 3))
 
 #6. Print b.
+print("\nEXERCISE 6 - Print b\n")
 print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
+print("\nEXERCISE 7 - Do a and b have the same size?")
+print("No, the haven't:\n")
 print(a.shape)
 print(b.shape)
 
 #8. Are you able to add a and b? Why or why not?
-print("\nNo, to add one matrix t another theirs shapes have to be equals\n")
+print("\nEXERCISE 8 - Are you able to add a and b? Why or why not?")
+print("No, to add one matrix t another theirs shapes have to be equals\n")
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
+print("\nEXERCISE 9 - Transpose b so that it has the same structure of a")
 c = b.reshape((2, 3, 5))
 print(np.shape(b))
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
+print("\nEXERCISE 10 - Try to add a and c. Now it should work. Assign the sum to variable d. But why does it work now?")
+print("Because a matrix and c matrix have the same dimensions\n")
 d = a + c
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-print('a')
-print(a)
-print('d')
-print(d)
-
+print("\nEXERCISE 11 - Print a and d. Notice the difference and relation of the two array in terms of the values? Explain")
 print("\nd matrix is the result of sum 1 to all theirs elements\n")
+print(a)
+print(d)
 
 #12. Multiply a and c. Assign the result to e.
 e = a * c
 
 #13. Does e equal to a? Why or why not?
-print('\na matrix:\n')
+print("\nEXERCISE 13 - Does e equal to a? Why or why not?")
+print("a matrix and e matrix are equals because the resul of multiply one matrix by identity matrix is the same matrix\n")
 print(a)
-print('\ne matrix:\n')
 print(e)
-
-print("\na matrix and e matrix are equals because the resul of multiply one matrix by identity matrix is the same matrix\n")
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 d_max = d.max()
 d_min = d.min()
 d_mean = d.mean()
 
+print("\nEXERCISE 14 - Identify the max, min, and mean values in d. Assign those values to variables d_max, d_min, and d_mean\n")
 print("Max: " + str(d_max))
 print("Min: " + str(d_min))
 print("Mean: " + str(d_mean))
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-f = np.empty((2, 3, 5))
-print('f')
-print(f)
+f = np.zeros([2, 3, 5])
 
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
@@ -77,6 +81,31 @@ Note: you don't have to use Numpy in this question.
 """
 
 
+'''
+# NOTE: THIS WAS MY FIRST APPROACH:
+
+for i, e in enumerate(d):
+    for j, el in enumerate(e):
+        for k, ele in enumerate(el):
+            if ele > d_min and ele < d_mean:
+                f[i][j][k] = 25
+            if ele >d_mean and ele < d_max:
+                f[i][j][k] = 75
+            if ele == d_mean:
+                f[i][j][k] = 50
+            if ele == d_min:
+                f[i][j][k] = 0
+            if ele == d_max:
+                f[i][j][k] = 100
+
+'''
+
+# BUT THIS APPROACH IS BETTER IMO:
+f[(d >  d_min) & (d < d_mean)] = 25
+f[(d >  d_mean) & (d < d_max)] = 75
+f[(d == d_mean)] = 50
+f[(d == d_min)] = 0
+f[(d == d_max)] = 100
 
 
 """
@@ -100,6 +129,10 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
 
+print("\nEXERCISE 17 - Print d and f. Do you have your expected f?\n")
+print(d)
+print(f)
+
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
@@ -113,3 +146,13 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+
+print("\nEXERCISE 18 - Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values (A, B, C, D, and E) to label the array elements?\n")
+f = f.astype(str)
+f[(d >  d_min) & (d < d_mean)] = "B"
+f[(d >  d_mean) & (d < d_max)] = "D"
+f[(d == d_mean)] = "C"
+f[(d == d_min)] = "A"
+f[(d == d_max)] = "E"
+
+print(f)
