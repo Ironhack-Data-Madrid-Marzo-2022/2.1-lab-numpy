8dd24b32eef0e187e0e0554a623c0afc46b12295
Migue Manzano <miguelmanzan@gmail.com>
2022-03-28 21:28:07 +0200

    Solve exercises

--- your-code/main.py
+++ your-code/main.py
@@ -1,69 +1,70 @@
 #1. Import the NUMPY package under the name np.
-
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
-
-
+print(np.__version__)
+# print(np.show_config())
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
-
+a=np.random.random((2, 3, 5))
 
 #4. Print a.
-
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
-
+b = np.ones((5, 2, 3))
 
 #6. Print b.
-
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
-
-
+print(a.shape)
+print(b.shape)
 
 #8. Are you able to add a and b? Why or why not?
-
-
+print("\nNo, to add one matrix t another theirs shapes have to be equals\n")
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
-
+c = b.reshape((2, 3, 5))
+print(np.shape(b))
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
-
+d = a + c
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
+print('a')
+print(a)
+print('d')
+print(d)
 
-
-
+print("\nd matrix is the result of sum 1 to all theirs elements\n")
 
 #12. Multiply a and c. Assign the result to e.
-
-
+e = a * c
 
 #13. Does e equal to a? Why or why not?
+print('\na matrix:\n')
+print(a)
+print('\ne matrix:\n')
+print(e)
 
-
-
+print("\na matrix and e matrix are equals because the resul of multiply one matrix by identity matrix is the same matrix\n")
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
+d_max = d.max()
+d_min = d.min()
+d_mean = d.mean()
 
-
-
+print("Max: " + str(d_max))
+print("Min: " + str(d_min))
+print("Mean: " + str(d_mean))
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
-
-
+f = np.empty((2, 3, 5))
+print('f')
+print(f)
 
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
